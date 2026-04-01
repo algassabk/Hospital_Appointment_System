38c4a8cae238fb34ebe3e99b95667d758c1b9495
algas <algassabk@gmail.com>
2026-04-01 16:57:17 +0300

    done with DepartmentDTO

--- src/main/java/com/hospital/dto/DepartmentDTO.java
+++ src/main/java/com/hospital/dto/DepartmentDTO.java
@@ -1,4 +1,16 @@
 package com.hospital.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DepartmentDTO {
+    private String name;
+    private String description;
 }
