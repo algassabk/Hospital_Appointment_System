edee449592064df521575f1476839c8d37ce0844
algas <algassabk@gmail.com>
2026-04-01 17:03:31 +0300

    finished DepartmentController

---
+++ src/main/java/com/hospital/controller/DepartmentController.java
@@ -0,0 +1,43 @@
+package com.hospital.controller;
+import com.hospital.dto.DepartmentDTO;
+import com.hospital.model.Department;
+import com.hospital.service.DepartmentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/departments")
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @PostMapping
+    public Department createDepartment(@RequestBody DepartmentDTO departmentDTO) {
+        return departmentService.createDepartment(departmentDTO);
+    }
+
+    @GetMapping
+    public List<Department> getAllDepartments() {
+        return departmentService.getAllDepartments();
+    }
+
+    @GetMapping("/{id}")
+    public Department getDepartmentById(@PathVariable Integer id) {
+        return departmentService.getDepartmentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Department updateDepartment(@PathVariable Integer id, @RequestBody DepartmentDTO departmentDTO) {
+        return departmentService.updateDepartment(id, departmentDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDepartment(@PathVariable Integer id) {
+        departmentService.deleteDepartment(id);
+    }
+}
