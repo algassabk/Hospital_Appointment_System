3ea7254e6363c53975ff51d3cf9ea6717978a0aa
algas <algassabk@gmail.com>
2026-04-05 19:35:50 +0300

    processing

--- src/main/java/com/hospital/dto/DepartmentDTO.java
+++ src/main/java/com/hospital/dto/DepartmentDTO.java
@@ -1,15 +1,31 @@
 package com.hospital.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class DepartmentDTO {
+
     private String name;
     private String description;
+
+    public DepartmentDTO() {
+    }
+
+    public DepartmentDTO(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/hospital/model/Department.java
+++ src/main/java/com/hospital/model/Department.java
@@ -1,15 +1,10 @@
 package com.hospital.model;
 
 import jakarta.persistence.*;
-import lombok.*;
 import java.util.List;
 
 @Entity
 @Table(name = "departments")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Department {
 
     @Id
@@ -24,4 +19,46 @@ public class Department {
 
     @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
     private List<Doctor> doctors;
+
+    public Department() {
+    }
+
+    public Department(Integer departmentId, String name, String description, List<Doctor> doctors) {
+        this.departmentId = departmentId;
+        this.name = name;
+        this.description = description;
+        this.doctors = doctors;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Doctor> getDoctors() {
+        return doctors;
+    }
+
+    public void setDoctors(List<Doctor> doctors) {
+        this.doctors = doctors;
+    }
 }
